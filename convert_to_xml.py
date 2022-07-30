91877b1b5a3186b9fb56f5a03682d8934725b636
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2022-07-31 03:44:04 +0800

    36

--- convert_to_xml.py
+++ convert_to_xml.py
@@ -3,6 +3,8 @@ import json
 import sys
 import time
 import gzip
+import os
+import binascii
 
 def ATTR_TYPE(attr:int):
 	o = ""
@@ -37,18 +39,45 @@ SPLIT_3RD_SIZE = 40000
 
 dedup_Table = []
 
-jsonData = json.loads(infile)
+Loaded_JSON = json.loads(infile)
 infile = None
 i = 1
-try: cid = jsonData["info"]["cid"]
+try: cid = Loaded_JSON["info"]["cid"]
 except KeyError: cid = 0
-try: maxlimit = jsonData["info"]["segment_count"]*6000
-except KeyError: maxlimit = 6000
-danmu_count = len(jsonData["elems"])
-XML_Data_1st_Cache = f"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\x0a<i>\x0a\t<chatserver>chat.bilibili.com</chatserver>\x0a\t<chatid>{cid}</chatid>\x0a\t<mission>0</mission>\x0a\t<maxlimit>{maxlimit}</maxlimit>\x0a\t<state>0</state>\x0a\t<real_name>0</real_name>\x0a\t<source>k-v</source>\x0a"
+try: Max_Limit = Loaded_JSON["info"]["segment_count"]*6000
+except KeyError: Max_Limit = 6000
+Danmaku_Count = len(Loaded_JSON["elems"])
+XML_Data_1st_Cache = f"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\x0a<i>\x0a\t<chatserver>chat.bilibili.com</chatserver>\x0a\t<chatid>{cid}</chatid>\x0a\t<mission>0</mission>\x0a\t<maxlimit>{Max_Limit}</maxlimit>\x0a\t<state>0</state>\x0a\t<real_name>0</real_name>\x0a\t<source>k-v</source>\x0a"
 XML_Data_2nd_Cache = ""
 XML_Data_3rd_Cache = ""
-for Sub_Item in jsonData["elems"]:
+
+try: Last_Modified_Time = Loaded_JSON['info']['File_Create_Time']
+except KeyError: Last_Modified_Time = int(os.stat(input_File).st_ctime)
+
+compatible_1 = False
+try:
+	Loaded_JSON["commandDms"]
+	compatible_1 = True
+except KeyError: pass
+if compatible_1:
+	for Item_sp in Loaded_JSON["commandDms"]:
+		id_ = Item_sp["id"]					# int64 id = 1
+		oid = Item_sp["oid"]				# int64 oid = 2;
+		mid = Item_sp['mid']				# int64 mid = 3;
+		command = Item_sp["command"]		# string command = 4;
+		content = Item_sp["content"]		# string content = 5;
+		try: progress = Item_sp["progress"]		# int32 progress = 6;
+		except KeyError: progress = 0
+		ctime = Item_sp["ctime"]			# string ctime = 7;
+		mtime = Item_sp["mtime"]			# string mtime = 8;
+		extra = Item_sp["extra"]			# string extra = 9;
+		idStr = Item_sp["idStr"]			# string idStr = 10
+		progress = format(progress/1000, ".5f")
+		sendtime = int(time.mktime(time.strptime(ctime, '%Y-%m-%d %H:%M:%S')))
+		midHash = hex(binascii.crc32(mid.encode())^0xFFFFFFFF).lstrip("0x").lstrip("0")
+		XML_Data_1st_Cache += f"\t<d p=\"{progress},1,25,16777215,{sendtime},999,{midHash},{id_},11\">{content}</d><!-- SPECIAL: {command}{extra} -->\x0a"
+
+for Sub_Item in Loaded_JSON["elems"]:
 	try: id_ = Sub_Item["id"]						# int64 id = 1;
 	except KeyError: id_ = "0"
 
@@ -57,7 +86,7 @@ for Sub_Item in jsonData["elems"]:
 	# 	continue
 	# dedup_Table.append(id_)
 
-	spec_tag = ""
+	Extended_Data = ""
 	try: progress = Sub_Item["progress"]			# int32 progress = 2;
 	except KeyError: progress = 0
 
@@ -104,7 +133,7 @@ for Sub_Item in jsonData["elems"]:
 	try: animation = Sub_Item["animation"]			# string animation = 22;
 	except KeyError: pass
 
-	try: replyCount = f"Reply:{Sub_Item['replyCount']} "												# test18
+	try: replyCount = f"Reply:{Sub_Item['replyCount']} "												# replyCount
 	except KeyError: replyCount = ""
 	dm_reply_to = " "
 	try: dm_reply_to = f"reply_to:{Sub_Item['test16']} "												# test16
@@ -118,13 +147,12 @@ for Sub_Item in jsonData["elems"]:
 					if Sub_Item['test21'] != "0": dm_reply_to = f"reply_to:{Sub_Item['test21']} "		# test21
 				except KeyError: pass
 
-	spec_tag = f"<!-- {attr}{usermid}{likes}{replyCount}{dm_reply_to}-->".replace("  "," ")
+	Extended_Data = f"<!-- {attr}{usermid}{likes}{replyCount}{dm_reply_to}-->".replace("  "," ")
 	progress = format(progress/1000, ".5f")
 	content = content.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\x00", " ").replace("\x08", " ").replace("\x14", " ").replace("\x17", " ").replace("\n", "\\n").replace("\r", "\\r")
 
 	# XML_item = "\t<d p=\"{0},{1},{2},{3},{4},{5},{6},{7},{8}\">{9}</d>{10}\x0a".format(progress, mode, fontsize, color, sendtime, pool, midHash, id_, weight, content, spec_tag)
-	XML_item = f"\t<d p=\"{progress},{mode},{fontsize},{color},{sendtime},{pool},{midHash},{id_},{weight}\">{content}</d>{spec_tag}\x0a"
-	XML_Data_3rd_Cache += XML_item
+	XML_Data_3rd_Cache += f"\t<d p=\"{progress},{mode},{fontsize},{color},{sendtime},{pool},{midHash},{id_},{weight}\">{content}</d>{Extended_Data}\x0a"
 	i += 1
 	if i % SPLIT_3RD_SIZE == 0:
 		XML_Data_1st_Cache += XML_Data_2nd_Cache
@@ -132,12 +160,9 @@ for Sub_Item in jsonData["elems"]:
 	if i % SPLIT_2ND_SIZE == 0:
 		XML_Data_2nd_Cache += XML_Data_3rd_Cache
 		XML_Data_3rd_Cache = ""
-		print(f"\rProgress: {1+len(dedup_Table)}|{i}/{danmu_count}, Time: {round(time.time()-Start_Time,3)}",end="")
+		print(f"\rProgress: {len(dedup_Table)}|{i}/{Danmaku_Count}, Time: {round(time.time()-Start_Time,3)}",end="")
 	Sub_Item = {}
 
-XML_Data_2nd_Cache += XML_Data_3rd_Cache
-XML_Data_1st_Cache += XML_Data_2nd_Cache
-XML_Data_1st_Cache += "</i>\x0a"
-open(outputFile, "w", encoding="utf-8").write(XML_Data_1st_Cache)
+open(outputFile, "w", encoding="utf-8").write(XML_Data_1st_Cache+XML_Data_2nd_Cache+XML_Data_3rd_Cache+f"</i>\x0a<!-- Create Time: {Last_Modified_Time} -->")
 End_Time = time.time()
-print(f"\r{1+len(dedup_Table)}|{danmu_count}, 总计用时：{round(End_Time-Start_Time, 4)}                     ")
+print(f"\r{len(dedup_Table)}|{Danmaku_Count}, 总计用时：{round(End_Time-Start_Time, 4)}                     ")
