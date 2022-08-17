fc9429fbf9b7a73818c62c341f649f3998f055c5
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2022-08-18 02:21:06 +0800

    50

--- my_lib/json2xml_Lib.py
+++ my_lib/json2xml_Lib.py
@@ -1,7 +1,7 @@
 from my_lib.attr import Danmaku_ATTR_TYPE
 
 
-def json2xml(this, exdata, enable_weight = False, All_Default = False):
+def json2xml(this, exdata, enable_weight = True):
 	"""
 	Text
 	"""
@@ -16,50 +16,48 @@ def json2xml(this, exdata, enable_weight = False, All_Default = False):
 	except KeyError: content = ""
 	try: sendtime = this["ctime"]
 	except KeyError: sendtime = "1262275200"
-	try: idStr = this["idStr"]
-	except KeyError: idStr = "0"
-	if id_ != idStr: print("id idStr mismatch:", id_, idStr)
-	if All_Default:
-		mode = "1"
-		fontsize = "25"
-		color = "16777215"
-		pool = "0"
-		weight = "9"
-	else:
-		try: mode: int = this["mode"]
-		except KeyError: mode = "0"
-		try: fontsize = this["fontsize"]
-		except KeyError: fontsize = "0"
-		try: color = this["color"]
-		except KeyError: color = "0"
-		try: pool = this["pool"]
-		except KeyError: pool = "0"
-		if enable_weight:
-			try: weight = this["weight"]
-			except KeyError: weight = "0"
-		else: weight = "9"
-		try: attr = Danmaku_ATTR_TYPE(this["attr"])
-		except KeyError: attr = "DM "
-		try: usermid = f"mid:{this['usermid']} "
-		except KeyError: usermid = ""
-		try: likes = f"Likes:{this['likes']} "
-		except KeyError: likes = ""
-		try: replyCount = f"Reply:{this['replyCount']} "
-		except KeyError: replyCount = ""
-		try: t16 = f"reply_to:{this['test16']} "
-		except KeyError: t16 = "0"
-		try: t17 = f"reply_to:{this['test17']} "
-		except KeyError: t17 = "0"
-		try: t20 = f"reply_to:{this['test20']} "
-		except KeyError: t20 = "0"
-		try: t21 = f"reply_to:{this['test21']} "
-		except KeyError: t21 = "0"
-		try: action = this["action"]
-		except KeyError: action = ""
-		try: animation = this["animation"]
-		except KeyError: animation = ""
-		if exdata: Extra_Data = f"<!-- {attr}{usermid}{likes}{replyCount}{proc_4(t16,t17,t20,t21)}-->".replace("  ", " ")
-
+	# try: idStr = this["idStr"]
+	# except KeyError: idStr = "0"
+	# if id_ != idStr: print("id idStr mismatch:", id_, idStr)
+	try: mode: int = this["mode"]
+	except KeyError:
+		if attr == 2: mode = "1"
+		else: mode = "0"
+	try: fontsize = this["fontsize"]
+	except KeyError:
+		if attr == 2: fontsize = "25"
+		else: fontsize = "0"
+	try: color = this["color"]
+	except KeyError:
+		if attr == 2: color = "16777215"
+		else: color = "0"
+	try: pool = this["pool"]
+	except KeyError: pool = "0"
+	if enable_weight:
+		try: weight = this["weight"]
+		except KeyError: weight = "0"
+	else: weight = "9"
+	try: attr = this["attr"]
+	except KeyError: attr = 0
+	try: usermid = f"mid:{this['usermid']} "
+	except KeyError: usermid = ""
+	try: likes = f"Likes:{this['likes']} "
+	except KeyError: likes = ""
+	try: replyCount = f"Reply:{this['replyCount']} "
+	except KeyError: replyCount = ""
+	try: t16 = f"reply_to:{this['test16']} "
+	except KeyError: t16 = "0"
+	try: t17 = f"reply_to:{this['test17']} "
+	except KeyError: t17 = "0"
+	try: t20 = f"reply_to:{this['test20']} "
+	except KeyError: t20 = "0"
+	try: t21 = f"reply_to:{this['test21']} "
+	except KeyError: t21 = "0"
+	try: action = this["action"]
+	except KeyError: action = ""
+	try: animation = this["animation"]
+	except KeyError: animation = ""
+	if exdata: Extra_Data = f"<!-- {Danmaku_ATTR_TYPE(attr)}{usermid}{likes}{replyCount}{proc_4(t16,t17,t20,t21)}-->".replace("  ", " ")
 	return f"\t<d p=\"{format(progress/1000, '.5f')},{mode},{fontsize},{color},{sendtime},{pool},{midHash},{id_},{weight}\">{content}</d>{Extra_Data}\n"
 
 def proc_4(a,b,c,d):
