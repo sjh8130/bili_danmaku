6472fa47ab4d84024f5a0e7dc4cfd9b90ab523f1
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2022-09-10 14:48:52 +0800

    57-

--- download_Embedded.py
+++ download_Embedded.py
@@ -11,19 +11,17 @@ x={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (K
 def p(b,c):
 	d=requests.get(b,headers=x)
 	print(f"[NET]:HTTP {d.status_code}\t{b}")
-	e=tarfile.TarInfo(f"[{t}]_[{c[0]}]_[{c[1]}]_[{c[2]}].{c[3]}")
+	e=tarfile.TarInfo(f"[{s}]_[{c[0]}]_[{c[1]}]_[{c[2]}].{c[3]}")
 	e.size=len(d.content)
 	e.mtime=time.time().__trunc__()
 	a.addfile(tarinfo=e,fileobj=io.BytesIO(d.content))
 	return d.content
 
-def q(a,b,c=""): return p(f'https://api.bilibili.com/x/v2/dm/web/seg.so?type=1&oid={a}&segment_index={b}',c=[a,"Danmaku",b+c,"bin"])
-
-def r():
+def q():
 	global a
-	with tarfile.open(name=t+".tar.gz",mode="w:gz",bufsize=4194304)as a:
-		b=json.loads(p(b=f"https://api.bilibili.com/x/web-interface/view?bvid={t}",c=["0","Video","INFO","json"]))["data"]
-		p(b=f"https://api.bilibili.com/x/web-interface/view/detail?bvid={t}",c=["0","Video","INFO_Detail","json"])
+	with tarfile.open(name=s+".tar.gz",mode="w:gz",bufsize=4194304)as a:
+		b=json.loads(p(b=f"https://api.bilibili.com/x/web-interface/view?bvid={s}",c=["0","Video","INFO","json"]))["data"]
+		p(b=f"https://api.bilibili.com/x/web-interface/view/detail?bvid={s}",c=["0","Video","INFO_Detail","json"])
 		c=b["title"]
 		d=b["pubdate"]
 		for e in b["subtitle"]["list"]:p(b=e["subtitle_url"],c=["0","Subs",f"{e['id']}_{e['lan']}","bcc"])
@@ -34,11 +32,11 @@ def r():
 			j=f'https://api.bilibili.com/x/v2/dm/web/view?type=1&oid={g}'
 			p(b=j,c=[g,"BAS","INFO","bin"])
 			k=str(f["part"])
-			print(f"{d}|{t}|av00000000|PXX/{len(b['pages'])}|{g}|{h}|{i}|{c}|{k}")
+			print(f"{d}|{s}|av00000000|PXX/{len(b['pages'])}|{g}|{h}|{i}|{c}|{k}")
 			for l in range(i):
-				try:q(g,str(l+1))
+				try:p(f'https://api.bilibili.com/x/v2/dm/web/seg.so?type=1&oid={g}&segment_index={str(l+1)}',c=[g,"Danmaku",str(l+1),"bin"])
 				except json.decoder.JSONDecodeError:pass
-				try:q(g,str(l+1),"_B1")
+				try:p(f'https://api.bilibili.com/x/v2/dm/web/seg.so?type=1&oid={g}&segment_index={str(l+1)}',c=[g,"Danmaku",str(l+1)+"_B1","bin"])
 				except json.decoder.JSONDecodeError:pass
 			m={}
 			m["elems"]=["Embedded"]
@@ -61,39 +59,39 @@ def r():
 			m["info"]["is_live_record"]=False
 			m["File_Ver"]="V3_20220819_Embedded"
 			n=json.dumps(m,ensure_ascii=False,separators=(',',':'))
-			o=tarfile.TarInfo(f"{t}_{g}.json")
+			o=tarfile.TarInfo(f"{s}_{g}.json")
 			o.size=len(n)
 			o.mtime=time.time().__trunc__()
 			a.addfile(tarinfo=o,fileobj=io.BytesIO(bytes(n,encoding="utf-8")))
 		a.close()
 
-def s():
+def r():
 	try:
 		a="{\"code\":0,\"data\":{\"archives\":[{\"aid\":0,\"pubdate\":1,\"bvid\":\"BV0000000000\"}]}}"
 		# a=requests.get("https://api.bilibili.com/x/series/archives?mid=xxxx&series_id=yyyy&only_normal=true&sort=desc&pn=1&ps=10",headers).content
 	except:
-		a="{\"code\":0,\"data\":{\"archives\":[{\"aid\":"+str(u)+",\"pubdate\":1,\"bvid\":\"BV0000000000\"}]}}"
+		a="{\"code\":0,\"data\":{\"archives\":[{\"aid\":"+str(t)+",\"pubdate\":1,\"bvid\":\"BV0000000000\"}]}}"
 		print("ERR",time.time().__trunc__())
 	if json.loads(a)["code"]!=0: print(f"[NET]:Error {time.time().__trunc__}")
 	return a
 
 if __name__=='__main__':
 	try:
-		t=sys.argv[1]
-		r()
+		s=sys.argv[1]
+		q()
 		print("With args")
 		sys.exit()
 	except IndexError:pass
-	u=v=w=0
+	t=u=v=0
 	while True:
-		v=json.loads(s())["data"]["archives"][0]
-		if u!=v["aid"]:
-			print(f"{v['pubdate']}|{v['bvid']}")
-			u=v["aid"]
-			if w==0:w=1
-			elif v==0:w=0
+		u=json.loads(r())["data"]["archives"][0]
+		if t!=u["aid"]:
+			print(f"{u['pubdate']}|{u['bvid']}")
+			t=u["aid"]
+			if v==0:v=1
+			elif u==0:v=0
 			else:
-				print(f"{int(time.time().__trunc__())}:{v['bvid']}")
-				t=v['bvid']
-				r()
+				print(f"{int(time.time().__trunc__())}:{u['bvid']}")
+				s=u['bvid']
+				q()
 		time.sleep(15)
