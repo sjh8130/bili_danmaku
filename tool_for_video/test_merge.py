57aa6c985fc31c9062299f9eee6d6e7e81dcafd1
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2023-02-10 07:22:12 +0800

    71

--- tool_for_video/test_merge.py
+++ tool_for_video/test_merge.py
@@ -1,5 +1,6 @@
 import json
 import io
+import os
 import sys
 
 """
@@ -17,67 +18,81 @@ for %a in (*.264) do ffprobe -v warning -hide_banner -show_data_hash SHA256 -sho
 for %a in (*.264) do ffprobe -v warning -hide_banner -show_data_hash SHA256 -show_packets -print_format json=compact=1 "%~na.264" > "%~naV.json"
 """
 
-input_file_1 = sys.argv[1]
-input_info_1 = sys.argv[1].rsplit(".",1)[-2]+sys.argv[3]+".json"
-input_file_2 = sys.argv[2]
-input_info_2 = sys.argv[2].rsplit(".",1)[-2]+sys.argv[3]+".json"
-output_file = "NUL"
-output_file = input_file_1.rsplit(".")[-2]+"_OUT."+input_file_1.rsplit(".")[-1]
+if sys.argv[3].__eq__("A"):
+	SUFFIX = ".aac"
+elif sys.argv[3].__eq__("V"):
+	SUFFIX = ".264"
+IN_INFO_1 = sys.argv[1].rsplit(".",1)[-2]+sys.argv[3]+".json"
+IN_INFO_2 = sys.argv[2].rsplit(".",1)[-2]+sys.argv[3]+".json"
+IN_FILE_1 = sys.argv[1].rsplit(".",1)[-2]+SUFFIX
+IN_FILE_2 = sys.argv[2].rsplit(".",1)[-2]+SUFFIX
 
-with open(input_info_1, "r") as hash_1, \
-	open(input_info_2, "r") as hash_2, \
-	io.open(input_file_1, "rb") as input_1, \
-	io.open(input_file_2, "rb") as input_2, \
-	io.open(output_file, "wb") as out:
+OUT__FILE = "NUL"
+OUT__FILE = IN_FILE_1.rsplit(".")[-2]+"_OUT."+IN_FILE_1.rsplit(".")[-1]
+
+with open(IN_INFO_1, "r") as HASH_1, \
+	open(IN_INFO_2, "r") as HASH_2, \
+	io.open(IN_FILE_1, "rb", buffering=256*1024*1024) as FILE_1, \
+	io.open(IN_FILE_2, "rb") as FILE_2, \
+	io.open(OUT__FILE, "wb") as OUT_FI:
 	print_control = 0
 	output_frames = 0
-	packets1 = json.load(hash_1)["packets"]
-	packets2 = json.load(hash_2)["packets"]
-	x = "========        "	# 输出文件1
-	y = "        ========"	# 输出文件2
-	z = "================"	# 文件1和文件2的帧内容相同，输出
+	output_F_size = 0
+	PKT_1 = json.load(HASH_1)["packets"]
+	PKT_2 = json.load(HASH_2)["packets"]
+	HASH_1.close()
+	HASH_2.close()
+	OUT_L = "========        "	# 输出文件1
+	OUT_R = "        ========"	# 输出文件2
+	OUT_A = "================"	# 文件1和文件2的帧内容相同，输出
+	LEN_P1 = len(PKT_1)
+	LEN_P2 = len(PKT_2)
 	i = 0
 	j = 0
+	print(f"{LEN_P1}+{LEN_P2} = {LEN_P1+LEN_P2}")
+	print(f"{os.stat(IN_FILE_1).st_size}+{os.stat(IN_FILE_2).st_size} = {os.stat(IN_FILE_1).st_size+os.stat(IN_FILE_2).st_size}")
 	while True:
-		a = packets1[i]
-		b = packets2[j]
-		if a["data_hash"] != b["data_hash"] and i != len(packets1)-1:
-			input_1.seek(int(a["pos"]))
-			out.write(input_1.read(int(a["size"])))
-			if print_control == 0:...
-			else: print()
+		a = PKT_1[i]
+		b = PKT_2[j]
+		if a["data_hash"] != b["data_hash"] and i != LEN_P1-1:
+			FILE_1.seek(int(a["pos"]))
+			OUT_FI.write(FILE_1.read(int(a["size"])))
+			if print_control != 0: print()
 			print_control = 0
 			output_frames += 1
-			print(x,output_frames,end="\r")
+			output_F_size += int(a["size"])
+			print(OUT_L,output_frames,end="\r")
 			i += 1
-		elif a["data_hash"] == b["data_hash"] and i != len(packets1)-1:
-			input_1.seek(int(a["pos"]))
-			out.write(input_1.read(int(a["size"])))
-			if print_control == 1:...
-			else: print()
+		elif a["data_hash"] == b["data_hash"] and i != LEN_P1-1:
+			FILE_1.seek(int(a["pos"]))
+			OUT_FI.write(FILE_1.read(int(a["size"])))
+			if print_control != 1: print()
 			print_control = 1
 			output_frames += 1
-			print(z,output_frames,end="\r")
+			output_F_size += int(a["size"])
+			print(OUT_A,output_frames,end="\r")
 			i += 1
 			j += 1
-		elif i == len(packets1)-1 and j != len(packets2)-1:
-			input_2.seek(int(b["pos"]))
-			out.write(input_2.read(int(b["size"])))
+		elif i == LEN_P1-1 and j != LEN_P2-1:
+			FILE_2.seek(int(b["pos"]))
+			OUT_FI.write(FILE_2.read(int(b["size"])))
 			output_frames += 1
-			if print_control == 2:...
-			else: print()
+			output_F_size += int(b["size"])
+			if print_control != 2: print()
 			print_control = 2
-			print(y,output_frames,end="\r")
+			print(OUT_R,output_frames,end="\r")
 			j += 1
-		elif i == len(packets1)-1 and j == len(packets2)-1:
-			input_2.seek(int(b["pos"]))
-			out.write(input_2.read(int(b["size"])))
+		elif i == LEN_P1-1 and j == LEN_P2-1:
+			FILE_2.seek(int(b["pos"]))
+			OUT_FI.write(FILE_2.read(int(b["size"])))
 			output_frames += 1
-			print(y,output_frames,end="\r")
+			output_F_size += int(b["size"])
+			print(OUT_R,output_frames,end="\r")
 			break
 		else:
 			print("ERR")
 			print(f"i={i}\tj={j}")
 			break
+	OUT_FI.close()
 	print()
-	print(output_frames)
+	print(output_frames, output_F_size)
---
+++ twicasting_comment_to_json.py
@@ -0,0 +1,67 @@
+import sys
+import time
+import bs4
+import lxml
+import json
+import ssl
+import requests
+ssl._create_default_https_context = ssl._create_unverified_context
+requests.packages.urllib3.disable_warnings()
+
+user = sys.argv[1]
+movie_id = sys.argv[2]
+try:
+	pages = int(sys.argv[3])
+except:
+	pages = 2
+
+TWITCASTING_URL_JP = "ja.twitcasting.tv"
+TWITCASTING_URL_EN = "en.twitcasting.tv"
+TWITCASTING_URL_GL = "twitcasting.tv"
+TWITCASTING_URL = TWITCASTING_URL_EN
+headers = {
+	'Host': TWITCASTING_URL,
+	'Connection': 'keep-alive',
+	'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.52"
+}
+
+
+def downloader(page, retry_times=0):
+	url = f"https://{TWITCASTING_URL}/{user}/moviecomment/{movie_id}-{page}"
+	print(url, retry_times)
+	try:
+		content = requests.get(url=url, headers=headers, verify=False, timeout=30).content
+	except KeyboardInterrupt:
+		print("BREAK")
+		return "BREAK"
+	except:
+		content = downloader(page, retry_times+1)
+	return content
+
+
+out = {"comment": [], "info": {"user": user, "movie_id": int(movie_id), "title": ""}}
+page = 0
+while(page <= int(pages)):
+	# a = open(f"twicasting_{user}_{movie_id}_{page}.html", encoding="utf-8").read()
+	a = downloader(page)
+	if a == "BREAK":
+		break
+	b = list(bs4.BeautifulSoup(a, "lxml").select(".tw-comment-history-item", limit=999))
+	# out["info"]["title"] = str(bs4.BeautifulSoup(a, "lxml").title.contents[0]).replace(" Comment - TwitCasting", "").replace(" コメント - ツイキャス", "")
+	if page == 0:
+		out["info"]["title"] = str(bs4.BeautifulSoup(a, "lxml").select(".tw-basic-page-header-path", limit=1)[0].contents[3].contents[1].contents[0]).rstrip(" ","")
+		pages = int(bs4.BeautifulSoup(a, "lxml").select(".tw-pager", limit=1)[0].contents[-1].contents[0])
+	for i in b:
+		out["comment"].append({
+			"id": int(i.attrs['data-comment-id']),
+			"time": int(time.mktime(time.strptime(i.select(".tw-comment-history-item__info__date")[0].attrs['datetime'], '%a, %d %b %Y %H:%M:%S %z'))),
+			"content": str(i.select(".tw-comment-history-item__content__text")[0].contents[0]).lstrip("\n").lstrip("\t").lstrip(" ").rstrip(" "),
+			"user_id": i.select(".tw-comment-history-item__details__user-link")[0].attrs['href'][1:],
+			"username": str(i.select(".tw-comment-history-item__details__user-link")[0].contents[0]).lstrip("\n").lstrip("\t").lstrip(" ").rstrip(" "),
+			"user_img": "https:"+i.select(".tw-comment-history-item__user__icon")[0].attrs['src']
+		})
+	page += 1
+
+out_json = json.dumps(out, ensure_ascii=False, separators=(",", ":"), indent="\t")
+# print(out_json)
+open(f"twicasting_{user}_{movie_id}.json", "w", encoding="utf-8").write(out_json)
