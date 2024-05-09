1c1c0c0894fa5d7111bae2fa0cde42c7002d8fff
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2024-05-10 01:29:15 +0800

    127

---
+++ jsontomd.py
@@ -0,0 +1,14 @@
+import json
+# 读取json数据
+f = input("input:")
+data = json.loads(f)
+# 初始化结果字符串
+try:
+	data["cmd"]
+	data = data["data"]
+except KeyError:
+	...
+res = f"| key | type | value |\n| - | - | - |\n"
+for key, value in data.items():
+	res += f"| {key} | {type(value).__name__.replace('int','num').replace('dict','obj').replace('list','[]')} | |\n"
+print(res)
