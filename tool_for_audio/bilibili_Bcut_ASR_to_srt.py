a6bfa1b772c5d3d12a1187952ea700bcec097b35
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2024-06-14 15:14:09 +0800

    131

---
+++ tool_for_audio/bilibili_Bcut_ASR_to_srt.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+from functools import lru_cache
+import json
+import sys
+
+
+@lru_cache
+def convert_srt_time(t: int):
+	return f"{(t//3600000):02d}:{(t//60000%60):02d}:{(t//1000%60):02d},{(t%1000):03d}"
+
+
+@lru_cache
+def convert_lrc_time(t: int):
+	return f"[{(t//60000):02d}:{(t//1000%60):02d}.{(t%1000//10):02d}]"
+
+
+@lru_cache
+def convert_ass_time(t: int):
+	return f"{(t//3600000):01d}:{(t//60000%60):02d}:{(t//1000%60):02d}.{(t%1000):03d}"[0:-1]
+
+
+def proc_karaoke(karaoke_item: dict):
+	karaoke_word = f"\x7b\\K{int((karaoke_item[0]["end_time"]-karaoke_item[0]["start_time"])/10)}\x7d{karaoke_item[0]["label"]}"
+	if karaoke_item[0]["label"].isascii():
+		karaoke_word += " "
+	for timed_chars in range(len(karaoke_item)):
+		if timed_chars == 0:
+			continue
+		karaoke_word += f"\x7b\\K{int((karaoke_item[timed_chars]["end_time"]-karaoke_item[timed_chars-1]["end_time"])/10)}\x7d{karaoke_item[timed_chars]["label"]}"
+		if karaoke_item[timed_chars]["label"].isascii() and timed_chars != len(karaoke_item):
+			karaoke_word += " "
+	return karaoke_word
+
+
+def proc_ASS(item: dict):
+	fi_k_itm = ""
+	start_time = item["start_time"]
+	end_time = item["end_time"]
+
+	fi___itm = f"Dialogue: 0,{convert_ass_time(start_time)},{convert_ass_time(end_time)},A,,0,0,0,,{item["transcript"]}\n"
+	try:
+		pass
+		# fi_k_itm = f"Dialogue: 1,{convert_ass_time(start_time)},{convert_ass_time(end_time)},B,,0,0,0,,{proc_karaoke(item["words"])}\n".replace("{\k0}","")
+	except KeyError:
+		pass
+	return fi___itm+fi_k_itm.replace(" \n", "\n").replace("  ", " ").replace(",,0,0,0,, ", ",,0,0,0,,")
+
+
+input_File = sys.argv[1]
+
+output_SRT = input_File.rsplit(".", 1)[-2]+"_P.srt"
+output_ASS = input_File.rsplit(".", 1)[-2]+"_P.ass"
+output_LRC = input_File.rsplit(".", 1)[-2]+"_P.lrc"
+output_TXT = input_File.rsplit(".", 1)[-2]+"_P.txt"
+input_File = open(input_File, "r", encoding="utf-8").read()
+
+Loaded_JSON = json.loads(input_File)
+del input_File
+srt_index = 0
+
+Final_SRT_Content = ""
+Final_LRC_Content = ""
+Final_TXT_Content = ""
+Final_ASS_Content = """[Script Info]
+Title: ASR subtitles
+ScriptType: v4.00+
+PlayResX: 1920
+PlayResY: 1080
+
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: A,Arial,60,&H00FFFFFF,&H0000FFFF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2,2,2,0,0,10,1
+Style: B,Arial,60,&H00FFFFFF,&H0000FFFF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2,2,2,0,0,10,1
+
+[Events]
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+"""
+aegisub_time_overflow = False
+lrc_time_overflow = False
+for line in Loaded_JSON["utterances"]:
+	srt_index += 1
+	Final_SRT_Content += f"{srt_index}\n{convert_srt_time(line["start_time"])} --> {convert_srt_time(line["end_time"])}\n{line["transcript"]}\n\n"
+	Final_LRC_Content += f"{convert_lrc_time(line["start_time"])}{line["transcript"]}\n"
+	Final_TXT_Content += f"{line["transcript"]}\n"
+	Final_ASS_Content += proc_ASS(line)
+	if line["start_time"] > 36000000 or line["end_time"] > 36000000:
+		aegisub_time_overflow = True
+	if line["start_time"] > 3600000:
+		lrc_time_overflow = True
+if lrc_time_overflow:
+	Final_LRC_Content += "[59:59.99]LRC时间溢出\n"
+if aegisub_time_overflow:
+	Final_ASS_Content += "Dialogue: 0,0:00:00.00,9:59:59.99,A,,0,0,0,,\{\\an2\}ASS时间溢出\n"
+# print(Final_SRT_Content)
+# print(Final_ASS_Content)
+# print(Final_LRC_Content)
+# print(Final_TXT_Content)
+open(output_SRT, "w", encoding="utf-8").write(Final_SRT_Content)
+# open(output_ASS, "w", encoding="utf-8").write(Final_ASS_Content)
+# open(output_LRC, "w", encoding="utf-8").write(Final_LRC_Content)
+# open(output_TXT, "w", encoding="utf-8").write(Final_TXT_Content)
--- tool_for_bili-live-dm/filter_livedm.py
+++ tool_for_bili-live-dm/filter_livedm.py
@@ -1,55 +1,66 @@
-import io
 import json
 import time
 import sys
-from filters import FILTER_WORDS,FILTER_USER,FILTER_USER_CRC
+from filters import FILTER_WORDS,FILTER_USER_ID,FILTER_USER_CRC_STR_LOWER
 
-line: str
-in_path = sys.argv[1]
-out_path = "Z:\\test.json"
-left_pos = 0
-dm_text: str
-danmaku: dict
-extra: dict
-start_time = time.time()
-try:
-	with open(out_path, "r", encoding="utf-8") as file_io:
-		final_write = json.load(file_io)
-except FileNotFoundError:
-	final_write = {}
-with open(in_path, "r", encoding="utf-8") as file_in:
-	for line in file_in.readlines():
-		if "DANMU_MSG" not in line: continue
-		if line.find("DANMU_MSG:3:7:1:1:1:1") == 1: continue
-		left_pos = line.find('{')
-		danmaku = json.loads(line[left_pos:-1])
 
-		if danmaku['info'][0][9] != 0: continue		# 1:节奏风暴 2:天选时刻 9:弹幕互动游戏
-		if danmaku['info'][0][12] != 0: continue	# 0:文本 1:表情包 2:语音
-
-		if danmaku['info'][0][7] in FILTER_USER_CRC: continue
-		if danmaku['info'][2][0] in FILTER_USER: continue
-
-		dm_text = danmaku['info'][1]
-		dm_text = dm_text.strip().strip("\u3000").strip("\u200e")
-		if dm_text in FILTER_WORDS or dm_text.lower() in FILTER_WORDS:
-			try:
-				del final_write[dm_text]
-			except:
-				pass
-			continue
+def main(in_paths, out_path):
+	line: str
+	left_pos = 0
+	dm_text: str
+	danmaku: dict
+	try:
+		with open(out_path, "r", encoding="utf-8") as file_io:
+			final_write = json.load(file_io)
+	except FileNotFoundError:
+		final_write = {}
+	except json.JSONDecodeError as e:
+		if e.args[0] == "Expecting value: line 1 column 1 (char 0)":
+			final_write = {}
 		else:
-			try:
-				if json.loads(danmaku['info'][0][15]['extra'])['hit_combo'] == 1: continue
-			except KeyError:
-				pass
-			try:
-				final_write[dm_text] += 1
-			except KeyError:
-				final_write[dm_text] = 1
+			raise e
+	with open(out_path, "w", encoding="utf-8") as file_io:
+		for in_path in in_paths:
+			print(in_path)
+			if in_path == out_path:
+				continue
+			with open(in_path, "r", encoding="utf-8") as file_in:
+				for line in file_in.readlines():
+					if "DANMU_MSG" not in line: continue
+					if line.find("DANMU_MSG:3:7:1:1:1:1") == 1: continue
+					left_pos = line.find('{')
+					danmaku = json.loads(line[left_pos:-1])
 
-with open(out_path, "w", encoding="utf-8") as file_io:
-	json.dump(final_write,file_io, ensure_ascii=False, indent="\t")
+					if danmaku['info'][0][9] != 0: continue		# 1:节奏风暴 2:天选时刻 9:弹幕互动游戏
+					if danmaku['info'][0][12] != 0: continue	# 0:文本 1:表情包 2:语音
 
-total_time = time.time() - start_time
-print(f"处理完成，耗时：{total_time:.3f}秒")
+					if danmaku['info'][0][7] in FILTER_USER_CRC_STR_LOWER: continue
+					if danmaku['info'][2][0] in FILTER_USER_ID: continue
+
+					dm_text = danmaku['info'][1]
+					dm_text = dm_text.strip().strip("\u3000").strip("\u200e")
+					if dm_text in FILTER_WORDS or dm_text.lower() in FILTER_WORDS:
+						try:
+							del final_write[dm_text]
+						except:
+							pass
+						continue
+					else:
+						try:
+							if json.loads(danmaku['info'][0][15]['extra'])['hit_combo'] == 1: continue
+						except KeyError:
+							pass
+						try:
+							final_write[dm_text] += 1
+						except KeyError:
+							final_write[dm_text] = 1
+		json.dump(final_write,file_io, ensure_ascii=False, indent="\t")
+
+if __name__ == "__main__":
+	in_path = sys.argv[1:]
+	out_path = "Z:\\test.json"
+	start_time = time.time()
+	main(in_path, out_path)
+	total_time = time.time() - start_time
+	print(f"处理完成，耗时：{total_time:.3f}秒")
+	time.sleep(5)
