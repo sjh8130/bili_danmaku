08c70c472b9510e1c0bf47402ace6b21c6e40e40
sjh8130 <31001963+sjh8130@users.noreply.github.com>
2024-09-04 20:03:12 +0800

    136

--- jsontomd.py
+++ jsontomd.py
@@ -5,11 +5,11 @@ f = input("input:")
 data = json.loads(f)
 # 初始化结果字符串
 try:
-	data["cmd"]
-	data = data["data"]
+    data["cmd"]
+    data = data["data"]
 except KeyError:
-	...
+    ...
 res = f"| key | type | value |\n| - | - | - |\n"
 for key, value in data.items():
-	res += f"| {key} | {type(value).__name__.replace('int','num').replace('dict','obj').replace('list','[]')} | |\n"
+    res += f"| {key} | {type(value).__name__.replace('int','num').replace('dict','obj').replace('list','[]')} | |\n"
 print(res)
